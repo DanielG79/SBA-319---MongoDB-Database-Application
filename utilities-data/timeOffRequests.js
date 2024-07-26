f9b2c4eb4c9679456f02f7d140d9d128bde97cac
Elucode <125608503+Elulove1@users.noreply.github.com>
2024-07-25 21:43:05 -0400

    Update timeOffRequests.js

---
+++ utilities-data/timeOffRequests.js
@@ -0,0 +1,10 @@
+const timeOffRequestsData = [
+
+    { id: 1, date: new Date("2024-01-01"), typeOffRequest: "Personal-Day", hoursADay: 8 },
+    { id: 2, date: new Date("2024-03-25"), typeOffRequest: "Vacation", hoursADay: 8 },
+    { id: 3, date: new Date("2024-06-10"), typeOffRequest: "Sick Leave", hoursADay: 3 },
+    { id: 4, date: new Date("2024-06-10"), typeOffRequest: "Parental Leave", hoursADay: 8 },
+    { id: 5, date: new Date("2024-06-10"), typeOffRequest: "Holiday Deferral", hoursADay: 8 }
+];
+
+module.exports = timeOffRequestsData;
